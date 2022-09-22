d4341f5632ac2e56332f70f674611640b8090fe8
Ирина <mi.irene97@gmail.com>
2022-09-22 17:43:01 +0300

    Вторая часть ДЗ

--- src/Main.java
+++ src/Main.java
@@ -21,5 +21,22 @@ public class Main {
             System.out.println(i);
         }
 
+        // ДЗ Часть 2
+        System.out.println("ДЗ Часть 2");
+        // Задание 1
+        System.out.println("Задание 1");
+        for (int i = 1904; i <= 2096; i = i + 4) {
+            System.out.println(i + " год является високосным.");
+        }
+        // Задание 2
+        System.out.println("Задание 2");
+        for (int i = 7; i <= 98; i = i + 7) {
+            System.out.println(i);
+        }
+        // Задание 3
+        System.out.println("Задание 3");
+        for (int i = 1; i <= 512; i = i * 2) {
+            System.out.println(i);
+        }
     }
 }
